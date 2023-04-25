96c14a07404f87fdf35f1381e940610d323cfd33
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2023-04-26 02:44:58 +0300

    Update and rename db_connection2.pas to db_connection2.py

---
+++ db_connection2.py
@@ -0,0 +1,13 @@
+#x = "import datetime import hashlib import sys import yaml from sqlalchemy import MetaData, create_engine from sqlalchemy.ext.declarative import declarative_base from sqlalchemy.orm import sessionmaker"
+#x1 = "class DBConnection: def __init__(self, config_path, schema): self.__db_config = self.__load_config(config_path) try: self.__db_user = self.__db_config.get('db_user') self.__db_passwd = self.__db_config.get('db_passwd') self.__db_name = self.__db_config.get('db_name') self.__db_host = self.__db_config.get('db_host') self.__db_port = self.__db_config.get('db_port') if 'db_port' in self.__db_config else 5432"
+#x2 = "sys.exit(1) self.__engine = create_engine('postgresql+psycopg2://%s:%s@%s:%d/%s' % (self.__db_user, self.__db_passwd, self.__db_host, int(self.__db_port), self.__db_name)"
+#x3 = "self.__schema = schema self.__metadata = MetaData(self.__engine, schema=self.__schema) self.__Base = declarative_base(metadata=self.__metadata) self.__Session = sessionmaker bind =(self.__engine) self.__session = self.__Session() def get_schema_name(self): return self.__schema def get_engine(self): return self.__engine def get_base(self): return self.__Base def get_metadata(self): return self.__metadata def get_session(self): return self.__session"
+#x4 = "@staticmethod def __load_config(config_pat if isinstance(config_path, dict): return config_path try: with open(config_path, 'r') as db_connection: try: return yaml.load(db_connection) except yaml.YAMLError as e: sys.exit('Error:{0}'.format(e)) except IOError as e: sys.exit('Error {0}) : {1}'.format(e.errno, e.strerror))"
+#x5 = "@staticmethod def get_hash_id(*args): to_md5 = ''.join(str(args)) id_hash  = hashlib.md5(to_md5).hexdigest() return id_has @staticmethod def get_date(date_format=''): return datetime.datetime.now() if date_format == '' else datetime.datetime.now().strftime(date_format)"
+
+#print("Unknown")
+#print("Unknown")
+#print("Unknown")
+#print("Unknown")
+#print("Unknown")
+#print("Unknown")
